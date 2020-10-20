b21c9b4dd931beecbe1a61a0ba17def2f39e6616
ayushic43 <ayushi140197@gmail.com>
2020-10-20 17:14:43 +0530

    Added Hi World

--- gitdemo/Program.cs
+++ gitdemo/Program.cs
@@ -7,6 +7,7 @@ namespace gitdemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi World!");
         }
     }
 }
